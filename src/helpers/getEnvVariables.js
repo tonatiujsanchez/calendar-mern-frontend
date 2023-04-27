c268f2e51e8808fef560d4cd0948b781fe34565a
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2023-04-27 03:06:18 -0600

    getEnvVariables actualizado

--- src/helpers/getEnvVariables.js
+++ src/helpers/getEnvVariables.js
@@ -2,12 +2,24 @@
 
 export const getEnvVariables = () => {
 
-    import.meta.env
+    // import.meta.env
   
-    return { ...import.meta.env }
-    
+    return { 
+        VITE_API_URL: import.meta.env.VITE_API_URL 
+    }
 }
 
+
+// export const getEnvVariables = () => {
+
+//     import.meta.env
+  
+//     return { ...import.meta.env }
+// }
+
+
+
+// Testing
 
 // export const getEnvVariables = () => {
 
